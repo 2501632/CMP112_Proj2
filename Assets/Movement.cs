2ce173b07ca6e98e980047b84d2123fbe2898f52
2501632 <2501632@uad.ac.uk>
2025-10-12 16:09:04 +0100

    movement input

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float moveSpeed;
+    float hInput;
+    float vInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +15,7 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        hInput = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        vInput = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
     }
 }
