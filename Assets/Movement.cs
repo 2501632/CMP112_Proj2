2b5abf3372f85802cedac04afb20f0e2268dbe48
2501632 <2501632@uad.ac.uk>
2025-11-11 19:20:09 +0000

    movement full

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -4,9 +4,14 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     public float moveSpeed;
+    public float jumpSpeed;
+    public float gravMult;
     float hInput;
     float vInput;
     public Rigidbody rb;
+    public LayerMask groundLayer;
+    public float groundDistance;
+    bool grounded;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -14,21 +19,36 @@ public class Movement : MonoBehaviour
 
     }
 
-    void OnMove(InputValue moveValue)
+    void Update()
     {
-        Vector2 moveVector = moveValue.Get<Vector2>();
+        grounded = Physics.Raycast(transform.position, Vector3.down, groundDistance, groundLayer);
 
-        hInput = moveVector.x;
-        vInput = moveVector.y;
+        if (Input.GetKeyDown(KeyCode.Space) && grounded == true)
+        {
+            
+            rb.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        }
+
+        if (rb.linearVelocity.y <= 0f)
+        {
+            rb.AddForce(Vector3.down * gravMult, ForceMode.Force);
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Vector3 move = new Vector3(hInput, 0.0f, vInput);
-        rb.AddForce(move * moveSpeed);
-
-        if (rb.linearVelocity.magnitude > moveSpeed)
-            rb.linearVelocity = rb.linearVelocity.normalized * moveSpeed;
+        hInput = Input.GetAxisRaw("Horizontal");
+        vInput = Input.GetAxisRaw("Vertical");
+        Vector3 moveInput = new Vector3(hInput, 0.0f, vInput);
+        transform.Translate(moveInput * moveSpeed * Time.deltaTime);
     }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * groundDistance);
+    }
+
 }
---
+++ Assets/Swinging.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class Swinging : MonoBehaviour
+{
+    public LineRenderer lr;
+    public LayerMask attachable;
+    public Transform cam, player, ropeLauncher;
+
+    public float maxSwingDistance;
+    Vector3 swingPoint;
+    SpringJoint joint;
+    Vector3 currentGrapplePos;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            StartSwing();
+        if (Input.GetKeyUp(KeyCode.Mouse0)) 
+            EndSwing();
+    }
+
+    void LateUpdate()
+    {
+        DrawRope();
+    }
+
+    void StartSwing()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(cam.position, cam.forward, out hit, maxSwingDistance, attachable))
+        {
+            swingPoint = hit.point;
+            joint = player.gameObject.AddComponent<SpringJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = swingPoint;
+
+            float distanceFromPoint = Vector3.Distance(player.position, swingPoint);
+
+            joint.maxDistance = distanceFromPoint * 0.8f;
+            joint.minDistance = distanceFromPoint * 0.25f;
+
+            joint.spring = 4.5f;
+            joint.damper = 7f;
+            joint.massScale = 4.5f;
+
+            lr.positionCount = 2;
+
+        }
+    }
+
+    void EndSwing()
+    {
+        lr.positionCount = 0;
+        Destroy(joint);
+    }
+
+    void DrawRope()
+    {
+        if (!joint)
+            return;
+
+        currentGrapplePos = Vector3.Lerp(currentGrapplePos, swingPoint, Time.deltaTime * 8f);
+
+        lr.SetPosition(0, ropeLauncher.position);
+        lr.SetPosition(1, swingPoint);
+    }
+}
